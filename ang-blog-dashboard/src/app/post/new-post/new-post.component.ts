5ac45a70d7330e34407fbdd06f72da30e39d9c19
Willian <willian.neibertz@gmail.com>
2023-12-08 13:09:12 -0300

    Dashboard new Post: Load <select> from firebase

--- ang-blog-dashboard/src/app/post/new-post/new-post.component.ts
+++ ang-blog-dashboard/src/app/post/new-post/new-post.component.ts
@@ -1,32 +1,37 @@
-import { LowerCasePipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CategoriesService } from 'src/app/services/categories.service';
 
 @Component({
   selector: 'app-new-post',
   templateUrl: './new-post.component.html',
   styleUrls: ['./new-post.component.css']
 })
-export class NewPostComponent {
-
+export class NewPostComponent implements OnInit {
 
   permalink: string = '';
   imgsrc: any = '../../../assets/landscape-placeholder-svgrepo-com.svg';
-  selectedImage :any;
+  selectedImage: any;
+  categories!: Observable<any>;
 
+  constructor(private categoryService: CategoriesService) { }
 
+  ngOnInit(): void {
+    this.categories = this.categoryService.loadData();
+  }
 
-onTitlechanged(event: any) {
-  const title = event.target.value
-  this.permalink = title.toLowerCase().replace(/\s/g, '-')
-}
+  onTitlechanged(event: any) {
+    const title = event.target.value
+    this.permalink = title.toLowerCase().replace(/\s/g, '-')
+  }
 
-showPreview(event: any) {
-const reader = new FileReader();
-reader.onload = (e)=>{
-  this.imgsrc = e.target?.result
-}
-reader.readAsDataURL(event.target.files[0]);
-this.selectedImage = event.target.files[0];
+  showPreview(event: any) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      this.imgsrc = e.target?.result
+    }
+    reader.readAsDataURL(event.target.files[0]);
+    this.selectedImage = event.target.files[0];
 
-}
+  }
 }
