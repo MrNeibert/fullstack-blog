6cd4bfd80801e1fbb7ca6f9fc8af85818f651efd
Willian <willian.neibertz@gmail.com>
2023-12-08 12:17:57 -0300

    Dashboard new post area

--- ang-blog-dashboard/src/app/post/new-post/new-post.component.ts
+++ ang-blog-dashboard/src/app/post/new-post/new-post.component.ts
@@ -1,3 +1,4 @@
+import { LowerCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-post.component.css']
 })
 export class NewPostComponent {
+
+  permalink: string = '';
+
+
+
+onTitlechanged(event: any) {
+  const title = event.target.value
+  this.permalink = title.toLowerCase().replace(/\s/g, '-')
+
+
+
+
+}
 
 }
