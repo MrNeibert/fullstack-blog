07c3a5170faa15e6896be77ccf090d2227d8fc5b
Willian <willian.neibertz@gmail.com>
2023-12-08 12:34:09 -0300

    Dashboard new post: Image loading

--- ang-blog-dashboard/src/app/post/new-post/new-post.component.ts
+++ ang-blog-dashboard/src/app/post/new-post/new-post.component.ts
@@ -8,17 +8,25 @@ import { Component } from '@angular/core';
 })
 export class NewPostComponent {
 
+
   permalink: string = '';
+  imgsrc: any = '../../../assets/landscape-placeholder-svgrepo-com.svg';
+  selectedImage :any;
 
 
 
 onTitlechanged(event: any) {
   const title = event.target.value
   this.permalink = title.toLowerCase().replace(/\s/g, '-')
-
-
-
-
 }
 
+showPreview(event: any) {
+const reader = new FileReader();
+reader.onload = (e)=>{
+  this.imgsrc = e.target?.result
+}
+reader.readAsDataURL(event.target.files[0]);
+this.selectedImage = event.target.files[0];
+
+}
 }
