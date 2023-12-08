cd6c1264ca75174a039f8dc9c08494c802801b02
Willian <willian.neibertz@gmail.com>
2023-12-07 21:40:30 -0300

    Update data functionality

--- ang-blog-dashboard/src/app/categories/categories.component.ts
+++ ang-blog-dashboard/src/app/categories/categories.component.ts
@@ -2,27 +2,44 @@ import { Component, OnInit } from '@angular/core';
 import { CategoriesService } from '../services/categories.service';
 import { Category } from '../models/category';
 import { Observable } from 'rxjs';
-import { collectionData } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css']
 })
-export class CategoriesComponent implements OnInit{
+export class CategoriesComponent implements OnInit {
 
-categoryArray !: Observable<any>;
+  categoryArray !: Observable<any>;
+  formCategory !: string;
+  formStatus: string = 'Add';
+  categoryId!: string
 
-  constructor( private categoryService: CategoriesService) { }
+  constructor(private categoryService: CategoriesService) { }
 
   ngOnInit(): void {
-this.categoryArray = this.categoryService.loadData();
+    this.categoryArray = this.categoryService.loadData();
   }
 
   onSubmit(formData: any) {
-    const categoryData :Category = { category: formData.value.category };
-    this.categoryService.saveData(categoryData);
-    formData.reset();
+
+    const categoryData: Category = { category: formData.value.category };
+
+    if (this.formStatus == 'Add') {
+      this.categoryService.saveData(categoryData);
+      formData.reset();
+    }
+    else if(this.formStatus == 'Edit'){
+      this.categoryService.updateData(this.categoryId, categoryData);
+      formData.reset();
+      this.formStatus = 'Add'
+    }
   }
 
+
+  onEdit(category: any, id: any) {
+    this.formCategory = category;
+    this.formStatus = 'Edit'
+    this.categoryId = id;
+  }
 }
--- ang-blog-dashboard/src/app/services/categories.service.ts
+++ ang-blog-dashboard/src/app/services/categories.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collection, addDoc, collectionData } from '@angular/fire/firestore'
+import { Firestore, collection, addDoc, collectionData, doc, updateDoc } from '@angular/fire/firestore'
 import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
@@ -7,21 +7,34 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class CategoriesService {
 
-  constructor(private fireStore : Firestore, private toastr: ToastrService) { }
+  constructor(private fireStore: Firestore, private toastr: ToastrService) { }
 
-  saveData(formData : any){
-      const collectionInstance = collection(this.fireStore, 'categories');
-      addDoc(collectionInstance, formData)
-      .then(()=> {
+  saveData(formData: any) {
+    const collectionInstance = collection(this.fireStore, 'categories');
+    addDoc(collectionInstance, formData)
+      .then(() => {
         this.toastr.success('Data Saved Successfully!')
-      } )
-      .catch((err)=> {
+      })
+      .catch((err) => {
         console.log(err);
       });
+  }
+
+  loadData() {
+    const collectionInstance = collection(this.fireStore, 'categories');
+    return collectionData(collectionInstance, {idField: 'id'})
+  }
+
+  updateData(id: string, updateData: any) {
+
+    const docInstance = doc(this.fireStore, 'categories', id);
+    updateDoc(docInstance, updateData)
+      .then(() => {
+        this.toastr.info('Data updated Successfully!')
+      })
+      .catch((err: any) => {
+        console.log(err);
+      });
+  }
 }
 
-loadData(){
-  const collectionInstance = collection(this.fireStore, 'categories');
-  return collectionData(collectionInstance)
-}
-}
